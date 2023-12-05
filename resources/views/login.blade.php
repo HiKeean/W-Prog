e7005bc66fcf0bc09e8a2c90f8a47dd4df21e50e
HiKeean <hizkiaalbertianhal@gmail.com>
2023-12-05 17:31:25 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>ShareStuff - Sharing is Caring kata Bapakmu</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/x-icon" href="{assets/images/logo/logo-4.png}">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/signericafat.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/cerebrisans.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/simple-line-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/elegant.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendor/linear-icon.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/easyzoom.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/plugins/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+
+<body>
+    <header class="header-area bg-orange">
+        <div class="header-large-device">
+            <div class="header-top header-top-ptb-7 border-bottom-9">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo">
+                                <a href="/"><img src="assets/images/logo/logo-4.png" alt="logo"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-small-device small-device-ptb-1">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-5">
+                        <div class="mobile-logo">
+                            <a href="/">
+                                <img alt="" src="assets/images/logo/logo-4.png">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-7">
+                        <div class="header-action header-action-flex">
+                            <div class="same-style-2 same-style-2-white same-style-2-hover-black same-style-2-font-inc">
+                                <a href="login-register.html"><i class="icon-user"></i></a>
+                            </div>
+                            <div class="same-style-2 same-style-2-white same-style-2-hover-black same-style-2-font-inc">
+                                <a href="wishlist.html"><i class="icon-heart"></i><span class="pro-count black">03</span></a>
+                            </div>
+                            <div class="same-style-2 same-style-2-white same-style-2-hover-black same-style-2-font-inc header-cart">
+                                <a class="cart-active" href="#">
+                                    <i class="icon-basket-loaded"></i><span class="pro-count black">02</span>
+                                </a>
+                            </div>
+                            <div class="same-style-2 same-style-2-white same-style-2-hover-black main-menu-icon">
+                                <a class="mobile-header-button-active" href="#"><i class="icon-menu"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="mobile-header-active mobile-header-wrapper-style">
+        <div class="col-xl-2 col-lg-2">
+            <div class="logo">
+                <a href="/"><img src="assets/images/logo/logo-4.png" alt="logo"></a>
+            </div>
+        </div>
+        <div class="clickalbe-sidebar-wrap">
+            <a class="sidebar-close"><i class="icon_close"></i></a>
+            <div class="mobile-header-content-area">
+                <div class="mobile-search mobile-header-padding-border-1">
+                    <form class="search-form" action="#">
+                        <input type="text" placeholder="Search here…">
+                        <button class="button-search"><i class="icon-magnifier"></i></button>
+                    </form>
+                </div>
+                <div class="mobile-menu-wrap mobile-header-padding-border-2">
+                    <nav>
+                        <ul class="mobile-menu">
+                            <li class="menu-item-has-children"><a href="/">Home</a>
+                            </li>
+                            <li class="menu-item-has-children "><a href="shop.html">Categories</a>
+                                <ul class="dropdown">
+                                    <li class="menu-item-has-children"><a href="#">shop layout</a>
+                                        <ul class="dropdown">
+                                            <option>All Categories </option>
+                                        <option>Alat Musik</option>
+                                        <option>Cosplay</option>
+                                        <option>Alat Pertukangan</option>
+                                        <option>Olahraga</option>
+                                            <li><a href="shop-list.html">Alat Musik</a></li>
+                                            <li><a href="shop-list.html">Cosplay</a></li>
+                                            <li><a href="shop-list.html">Olahraga</a></li>
+                                            <li><a href="shop-list.html">Alat Pertukangan</a></li>
+                                            <li><a href="shop-list.html">Rumah Tangga</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="#">Products Layout</a>
+                                        <ul class="dropdown">
+                                            <li><a href="product-details.html">tab style 1</a></li>
+                                            <li><a href="product-details-2.html">tab style 2</a></li>
+                                            <li><a href="product-details-sticky.html">sticky style</a></li>
+                                            <li><a href="product-details-gallery.html">gallery style </a></li>
+                                            <li><a href="product-details-affiliate.html">affiliate style</a></li>
+                                            <li><a href="product-details-group.html">group style</a></li>
+                                            <li><a href="product-details-fixed-img.html">fixed image style </a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children"><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="about-us.html">about us </a></li>
+                                    <li><a href="cart.html">cart page</a></li>
+                                    <li><a href="checkout.html">checkout </a></li>
+                                    <li><a href="my-account.html">my account</a></li>
+                                    <li><a href="wishlist.html">wishlist </a></li>
+                                    <li><a href="compare.html">compare </a></li>
+                                    <li><a href="contact.html">contact us </a></li>
+                                    <li><a href="order-tracking.html">order tracking</a></li>
+                                    <li><a href="login-register.html">login / register </a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children "><a href="#">Blog</a>
+                                <ul class="dropdown">
+                                    <li><a href="blog.html">blog standard </a></li>
+                                    <li><a href="blog-no-sidebar.html">blog no sidebar </a></li>
+                                    <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
+                                    <li><a href="blog-details.html">blog details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="mobile-social-icon">
+                    <a class="facebook" href="#"><i class="icon-social-facebook"></i></a>
+                    <a class="twitter" href="#"><i class="icon-social-twitter"></i></a>
+                    <a class="pinterest" href="#"><i class="icon-social-pinterest"></i></a>
+                    <a class="instagram" href="#"><i class="icon-social-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="container ">
+            <div class="text-center mt-4 name">
+                <h1>
+                    Login
+                </h1>  
+            </div>
+            <form class="p-3 mt-3">
+                <div class="form-field d-flex align-items-center">
+                    <span class="far fa-user"></span>
+                    <input type="text" name="userName" id="userName" placeholder="Username">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <span class="fas fa-key"></span>
+                    <input type="password" name="password" id="pwd" placeholder="Password">
+                </div>
+                <br>
+                <button type="button" class="btn btn-primary"> Login </button>
+            </form>
+            <div class="text-center fs-6">
+                <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+            </div>
+        </div>
+        
+    </div>
+</body>
